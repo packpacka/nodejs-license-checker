db7d676774f3c6e3adaaa4b96a68728b3c1d8652
Evgeny Chirko <zhenya_chirko@mail.ru>
2021-05-06 18:49:18 +0300

    Add custom license-checker

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const licenseChecker = require('license-checker')
 
-try {
-  const allowedLicenses = core.getInput('allow-only')
+const checkLicenses = (path) => {
+  return new Promise((resolve) => {
+    const allowedLicenses = core.getInput('allow-only')
+    try {
+      licenseChecker.init({
+        start: path,
+        onlyAllow: allowedLicenses,
+        excludePackages: excludePackages.join(';')
+      }, err => {
+        resolve(err);
+      })
+    } catch (error) {
+      resolve(error);
+    }
+  });
+}
 
-  licenseChecker.init({
-    start: './',
-    onlyAllow: allowedLicenses,
-  }, err => err && core.setFailed(err.message))
+try {
+  const paths = (core.getInput('paths') || './').split(';');
+
+  Promise.all(paths.map(checkLicenses)).then((errors) => {
+    if (errors.filter(Boolean).length) {
+      errors.forEach((error, index) => {
+        if (error) {
+          console.log(`Found errors while check path "${paths[index]}"`);
+          console.log(error);
+        }
+      })
+      core.setFailed('Licenses check has been failed')
+    }
+  })
 } catch (error) {
   core.setFailed(error.message)
 }
